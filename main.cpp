951ce831e2e410229cd0a02f06c01dd913474738
Rajesh Pandian M <mrpraj@gmail.com>
2022-02-26 01:46:07 +0530

    Added files

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//~~~START:Sat, 26-Feb-2022, 01:44:32 IST
+//~~~Author:Rajesh Pandian M | mrprajesh.co.in
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char* argv[]){
+  std::cout << "Hello Dear Programmer! \n\n --cpp\n";
+  return 0;
+}
